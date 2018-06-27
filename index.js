c903603371586f7d2c3ffd229e361cbcc76594c4
Marissa Zhong <mwz2101@columbia.edu>
2018-06-27 12:56:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,28 @@ function findAdjacent(stationName, vertices, edges) {
   return results;
 }
 
+edges = [
+  ['14th&6th', '23rd&6th'],
+  ['23rd&6th', '34th&6th'],
+  ['34th&6th', '28th&Bwy'],
+  ['28th&Bwy', '23rd&Bwy'],
+  ['23rd&Bwy', '14th&Lex'],
+  ['14th&Lex', '23rd&Lex']
+]
+
+vertices = [
+  {name: '34th&6th', distance: null, predecessor: null},
+  {name: '23rd&6th', distance: null, predecessor: null},
+  {name: '28th&Bwy', distance: null, predecessor: null},
+
+  {name: '14th&6th', distance: null, predecessor: null},
+  {name: '23rd&Bwy', distance: null, predecessor: null},
+  {name: '14th&Lex', distance: null, predecessor: null},
+  {name: '23rd&Lex', distance: null, predecessor: null}
+]
+
+console.log(findAdjacent('34th&6th',vertices,edges));
+
 function markDistanceAndPredecessor() {
 
 }
