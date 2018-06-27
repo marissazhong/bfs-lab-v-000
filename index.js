e6019e246709f14c7eeb9f65df74486a1cc05f83
Marissa Zhong <mwz2101@columbia.edu>
2018-06-27 12:49:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ function bfs(rootNode, vertices, edges){
 }
 
 function findAdjacent(stationName, vertices, edges) {
-  
+  let results = []
+  for (let i = 0; i < edges.length; i++) {
+    if(edges[i][0] == stationName) {
+      
+    }
+  }
 }
 
 function markDistanceAndPredecessor() {
-  
+
 }
 
 function addToQueue() {
-  
+
 }
