98df43c81a61ecb94ee6d33b6d776f880a87d9d2
Marissa Zhong <mwz2101@columbia.edu>
2018-06-27 12:50:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function findAdjacent(stationName, vertices, edges) {
     if(edges[i][0] == stationName) {
       results.push(vertices.edges[i][1]);
     } else if (edges[i][1] == stationName) {
-      results.push(vertiecs.edges[i][0]);
+      results.push(vertices.edges[i][0]);
     }
   }
   return results;
