becfcd9c4ce563cbe54052a2861158c4e5399ac1
Marissa Zhong <mwz2101@columbia.edu>
2018-06-27 13:20:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,32 +30,32 @@ function findAdjacent(stationName, vertices, edges) {
   return results;
 }
 
-edges = [
-  ['14th&6th', '23rd&6th'],
-  ['23rd&6th', '34th&6th'],
-  ['34th&6th', '28th&Bwy'],
-  ['28th&Bwy', '23rd&Bwy'],
-  ['23rd&Bwy', '14th&Lex'],
-  ['14th&Lex', '23rd&Lex']
-]
-
-vertices = [
-  {name: '34th&6th', distance: null, predecessor: null},
-  {name: '23rd&6th', distance: null, predecessor: null},
-  {name: '28th&Bwy', distance: null, predecessor: null},
-
-  {name: '14th&6th', distance: null, predecessor: null},
-  {name: '23rd&Bwy', distance: null, predecessor: null},
-  {name: '14th&Lex', distance: null, predecessor: null},
-  {name: '23rd&Lex', distance: null, predecessor: null}
-]
-
-
-findAdjacent('34th&6th',vertices,edges);
-let thirtyFourthAndSixth = vertices[0]
-thirtyFourthAndSixth.distance = 0
-
-console.log(findAdjacent('28th&Bwy',vertices,edges));
+// edges = [
+//   ['14th&6th', '23rd&6th'],
+//   ['23rd&6th', '34th&6th'],
+//   ['34th&6th', '28th&Bwy'],
+//   ['28th&Bwy', '23rd&Bwy'],
+//   ['23rd&Bwy', '14th&Lex'],
+//   ['14th&Lex', '23rd&Lex']
+// ]
+//
+// vertices = [
+//   {name: '34th&6th', distance: null, predecessor: null},
+//   {name: '23rd&6th', distance: null, predecessor: null},
+//   {name: '28th&Bwy', distance: null, predecessor: null},
+// 
+//   {name: '14th&6th', distance: null, predecessor: null},
+//   {name: '23rd&Bwy', distance: null, predecessor: null},
+//   {name: '14th&Lex', distance: null, predecessor: null},
+//   {name: '23rd&Lex', distance: null, predecessor: null}
+// ]
+//
+//
+// findAdjacent('34th&6th',vertices,edges);
+// let thirtyFourthAndSixth = vertices[0]
+// thirtyFourthAndSixth.distance = 0
+//
+// console.log(findAdjacent('28th&Bwy',vertices,edges));
 
 function markDistanceAndPredecessor() {
 
